b2d13c6c168d21516643c7580eb58130ab20319d
sir_nacnud <duncanc4@gmail.com>
2016-08-05 18:30:36 +0200

    Add setup files for pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'parse-schema-validator',
+  packages = ['parse-schema-validator'],
+  version = '1.0.0',
+  description = 'Tool for validating Parse classes on a Parse Server',
+  author = 'Duncan Cunningham',
+  author_email = 'duncanc4@gmail.com',
+  license = "MIT",
+  url = 'https://github.com/sirnacnud/parse-schema-validator',
+  download_url = 'https://github.com/sirnacnud/parse-schema-validator/archive/1.0.0.tar.gz',
+  keywords = ['parse', 'schema', 'validator'],
+  classifiers = [],
+  data_files = [("", ["license"])]
+)
