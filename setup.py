a0a8c3ec083b60d88b06b346071dee6b3f4eb736
Duncan Cunningham <duncanc4@gmail.com>
2016-08-14 20:57:07 +0200

    Bumped to version 1.0.3

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     entry_points={
         'console_scripts': ['parse-schema-validator=parse_schema_validator.schema_validator:main']
     },
-    version='1.0.2',
+    version='1.0.3',
     description='Tool for validating Parse classes on a Parse Server',
     author='Duncan Cunningham',
     author_email='duncanc4@gmail.com',
     license='MIT',
     url='https://github.com/sirnacnud/parse-schema-validator',
-    download_url='https://github.com/sirnacnud/parse-schema-validator/archive/1.0.2.tar.gz',
+    download_url='https://github.com/sirnacnud/parse-schema-validator/archive/1.0.3.tar.gz',
     keywords=['parse', 'schema', 'validator'],
     classifiers=[],
 )
